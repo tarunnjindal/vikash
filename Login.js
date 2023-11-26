9fcb9246ebb782a46f48dee6b2c8ac81495a466b
tarunnjindal <143988124+tarunnjindal@users.noreply.github.com>
2023-11-26 23:24:02 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,13 @@
+// javascript code !
+
+function abc(){
+ var x=document.getElementById("one").value;
+ var y=document.getElementById("two").value;
+ if(x==="cse326" && y==="k22pf")
+ {
+    window.alert("Login succesful !");
+ }
+ else{
+    window.alert("Login failed !");
+ }
+}
